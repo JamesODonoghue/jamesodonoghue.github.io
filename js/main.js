87d263f31114a29a5fa3eb6d2dea6ef0f68c0c1e
James O'Donoghue <jodonogh1@gmail.com>
2019-08-17 21:34:31 -0700

    big redesign to landing page

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     })
+
+    $('.project-item').mouseover(function(e){
+        $('.project-info').toggleClass('hidden')
+        $('.project-title').toggleClass('hidden')
+    })
+    $('.project-item').mouseout(function(e){
+        $('.project-info').toggleClass('hidden')
+        $('.project-title').toggleClass('hidden')
+    })
 })
