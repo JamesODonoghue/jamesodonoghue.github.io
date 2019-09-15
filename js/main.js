cbc6a3f5630b825b6f4c2b84a11094940e0151c8
James O'Donoghue <jodonogh1@gmail.com>
2019-09-15 13:04:49 -0700

    another large redesign, simplified layout. added a bus svg icon.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function(){
-    $('.menu-toggler').click(function(){
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
-    })
+    // $('.menu-toggler').click(function(){
+    //     $(this).toggleClass('open');
+    //     $('.top-nav').toggleClass('open');
+    // })
 
-    $('.project-item').mouseover(function(e){
-        $('.project-info').toggleClass('hidden')
-        $('.project-title').toggleClass('hidden')
-    })
-    $('.project-item').mouseout(function(e){
-        $('.project-info').toggleClass('hidden')
-        $('.project-title').toggleClass('hidden')
-    })
+    // $('.project-item').mouseover(function(e){
+    //     $('.project-info').toggleClass('hidden')
+    //     $('.project-title').toggleClass('hidden')
+    // })
+    // $('.project-item').mouseout(function(e){
+    //     $('.project-info').toggleClass('hidden')
+    //     $('.project-title').toggleClass('hidden')
+    // })
 })
