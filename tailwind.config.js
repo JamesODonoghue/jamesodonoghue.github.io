02fbbdc3737c49415794da6f0a4030f296a9de09
James O'Donoghue <jameso@James-ODonoghue-Macbook-Pro-2.local>
2022-10-29 21:16:49 -0700

    chore: style cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,12 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-    purge: [],
     darkMode: 'class',
+    content: ['index.html'],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Inter'],
                 serif: ['Recoleta'],
             },
-            colors: {
-                teal: colors.teal,
-                orange: colors.orange,
-                gray: colors.blueGray,
-                cyan: colors.cyan,
-            },
         },
     },
-    variants: {
-        extend: {
-            textColor: ['hover'],
-        },
-    },
-    plugins: [],
 }
